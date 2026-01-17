40c14a2bde31d8acb63bd4ace959b4e2c39278b6
janiruchit <janiruchit3@gmail.com>
2026-01-17 14:33:52 +0530

    Refactor notificationclick event handling

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-});
+self.addEventListener('install', (e) => self.skipWaiting());
 
 self.addEventListener('notificationclick', (e) => {
     e.notification.close();
-    e.waitUntil(clients.openWindow('/'));
+    e.waitUntil(clients.matchAll({type: 'window'}).then(clients => {
+        if (clients.length > 0) return clients[0].focus();
+        return clients.openWindow('/');
+    }));
 });
 
 self.addEventListener('fetch', (e) => {
